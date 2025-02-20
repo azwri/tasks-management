30f2d1c341e8793236f59ab68ee96d9822eeeba3
azwri <aazwri@gmail.com>
2025-02-20 08:50:04 +0300

    setup nextjs frontend

---
+++ frontend/src/app/components/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary border">
+            <div className="container">
+            <a className="navbar-brand" href="#">Tasks</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav ms-auto">
+                    <a className="nav-item nav-link active" href="#">Login</a>
+                    <a className="nav-item nav-link" href="#">About</a>
+                </div>
+            </div>
+            </div>
+        </nav>
+    );
+}
